1af4c1f11e61a042d027eb0e9b9762f772ea278d
sh5054 <sh95054@163.com>
2016-03-26 09:37:19 +0800

    no message

---
+++ 微信接口/gettoken.php
@@ -0,0 +1,27 @@
+<?php
+	function httpGet($url) {
+	    $curl = curl_init();
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+	    // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
+	    // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
+	    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+	    // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+	    curl_setopt($curl, CURLOPT_URL, $url);
+
+	    $res = curl_exec($curl);
+	    curl_close($curl);
+
+	    return $res;
+	  }
+
+
+	$appid = "wx9cee*****40454b";
+	$appsecret = "f5822eef65d1*****118fcbecc67fa";
+	$tokenapi = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appid}&secret={$appsecret}";
+//	$str = file_get_contents($tokenapi); //不稳定 ,微信推荐使用curl()
+	$str = httpGet($tokenapi);
+	$token = json_decode($str);
+	$token = $token->access_token;
+	echo $token;
+?>
